70ec3df1676e8d09236367232272668d632d8bce
Fr. Romain <editeur@traditions-monastiques.com>
2017-11-28 15:08:15 +0100

    Ste Cécile and St Clément modified.

--- functions/sancto.py
+++ functions/sancto.py
@@ -1648,14 +1648,14 @@ def dict_sancto_create(current_year, even_year, year_letter, dict_tempo, paques,
     sainte_cecile = dict_sancto[sainte_cecile_date] = {}
     sainte_cecile["force"] = 40
     sainte_cecile["header"] = " - \\textsc{S. Cæciliæ}, virginis et martyris - \\textbf{memoria maior} - \\textit{Rub.}"
-    sainte_cecile["body"] = "\n\\item ad Vigilias pro breviario vetere: in supplemento 191.\n\\item ad Laudes, Vesperas et Horas minores: antiphonæ propriæ.\n\\item in MC: omnia in MP ; lectiones propriæ: Apoc \\textbf{19}, 1.5-9a / Mt \\textbf{25}, 1-13 ; præfatio de sanctis martyribus."
+    sainte_cecile["body"] = "\n\\item ad Vigilias pro breviario vetere: in supplemento 191.\n\\item ad Laudes, Vesperas et Horas minores: antiphonæ propriæ.\n\\item in MC: omnia in MP ; lectiones propriæ: Os \\textbf{2}, 16b.17b.21-22 /  \\textbf{25}, 1-13 ; præfatio de sanctis martyribus."
     sainte_cecile["II_vesp"] = "\n\\item ad Magnificat: ø \\textit{Est secretum} (AM 1139)."
     
     saint_clement_date = datetime.date(current_year, 11, 23)
     saint_clement = dict_sancto[saint_clement_date] = {}
     saint_clement["force"] = 40
     saint_clement["header"] = " - \\textsc{S. Clementis I}, papæ et martyris - \\textbf{memoria maior} - \\textit{Rub.}"
-    saint_clement["body"] = "\n\\item ad Laudes, Vesperas et Horas minores: antiphonæ propriæ.\n\\item in MC: Commune martyrum (MR 917) ; lectiones propriæ: 1 P \\textbf{5}, 10-13 / Mt \\textbf{16}, 12-17; præfatio de sanctis martyribus."
+    saint_clement["body"] = "\n\\item ad Laudes, Vesperas et Horas minores: antiphonæ propriæ.\n\\item in MC: Commune martyrum (MR 917) ; lectiones propriæ: 1 P \\textbf{5}, 1-4 / Mt \\textbf{16}, 13-19; præfatio de sanctis martyribus."
     saint_clement["II_vesp"] = "\n\\item ad Vesperas: a capitulo ut in II Vesperis de Communi unius martyris ; ad Magnificat: ø \\textit{Dedisti} (AM 1148)."
     
     saint_andre_dung_lac_date = datetime.date(current_year, 11, 24)
